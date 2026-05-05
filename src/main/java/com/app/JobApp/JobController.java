f72a65bf69dc2cb01d0d564a2f063f230a0928f3
Ali Ahmed ElKaramany <aliahmed999416@gmail.com>
2026-05-05 21:53:05 +0300

    Adding viewAllJobs handler to view all jobs on the portal

--- src/main/java/com/app/JobApp/JobController.java
+++ src/main/java/com/app/JobApp/JobController.java
@@ -4,9 +4,13 @@ package com.app.JobApp;
 import com.app.JobApp.model.JobPost;
 import com.app.JobApp.service.JobService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.Banner;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.List;
 
 @Controller
 public class JobController {
@@ -27,8 +31,15 @@ public class JobController {
     @PostMapping("/handleForm")
     public String handleFrom(JobPost newJobPost){
         service.addJob(newJobPost);
-        System.out.println(service.getAllJobs());
         return "success";
+    }
+
+
+    @GetMapping("viewAllJobs")
+    public String viewJobs(Model model){
+        List<JobPost> jobs = service.getAllJobs();
+        model.addAttribute("jobPosts", jobs);
+        return"viewAllJobs";
     }
 
 }
