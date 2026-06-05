9e353ef684e057a365b7cd52732ee0ec5ebf966d
Ali Ahmed ElKaramany <aliahmed999416@gmail.com>
2026-06-05 19:27:48 +0300

    JobApp Project using REST

---
+++ src/main/java/com/app/spring_boot_rest/JobRestController.java
@@ -0,0 +1,50 @@
+package com.app.spring_boot_rest;
+
+import com.app.spring_boot_rest.model.JobPost;
+import com.app.spring_boot_rest.service.JobService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@CrossOrigin(origins = "http://localhost:3000")
+public class JobRestController {
+
+    @Autowired
+    private JobService service;
+
+
+    @GetMapping("jobPosts")
+    public List<JobPost> getAllPosts(){
+        return service.getAllJobs();
+    }
+
+    @GetMapping("jobPost/{postId}")
+    public JobPost getJob(@PathVariable("postId") int postId)
+    {
+        return service.getJob(postId);
+    }
+
+    @PostMapping("jobPost")
+    public JobPost addJob(@RequestBody JobPost jobPost){
+        service.addJob(jobPost);
+        return service.getJob(jobPost.getPostId());
+    }
+
+
+    @PutMapping("jobPost")
+    public JobPost updateJob(@RequestBody JobPost jobPost){
+        service.updateJob(jobPost);
+
+        return service.getJob(jobPost.getPostId());
+    }
+
+    @DeleteMapping("jobPost/{postId}")
+    public String deleteJob(@PathVariable("postId") int postId){
+        service.deleteJob(postId);
+        return "Deleted";
+    }
+
+}
